67cba715ddcb5a431e0f1cda48dd6474031f25bb
Gaksy <gaksys@hotmail.com>
2024-05-12 23:32:50 +0800

    移除或修复产生警告的代码

--- src/main/java/com/laelioa/cbmod/blocks/inception/BlockConcrete.java
+++ src/main/java/com/laelioa/cbmod/blocks/inception/BlockConcrete.java
@@ -6,12 +6,8 @@ import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
